8c5ba525368c251c98bb027acc0113ded067a31b
peterdowney <peter@downeydesigns.co.uk>
2023-08-29 14:34:10 +0100

    removed silly section making all table th td have the same classes

--- TableMaker/TableMaker.js
+++ TableMaker/TableMaker.js
@@ -1,24 +1,22 @@
 
 class TableMaker {
 
-    static generateTableHead(table, data, classes = []) {
+    static generateTableHead(table, data) {
         let thead = table.createTHead();
         let row = thead.insertRow();
         for (let key of data) {
             let th = document.createElement("th");
-            for(let style of classes) th.classList.add(style);
             let text = document.createTextNode(key);
             th.appendChild(text);
             row.appendChild(th);
         }
     }
     
-    static generateTable(table, data, classes = []) {
+    static generateTable(table, data) {
         for (let element of data) {
             let row = table.insertRow();
             for (let key in element) {
                 let cell = row.insertCell();
-                for(let style of classes) cell.classList.add(style);
                 let text = document.createTextNode(element[key]);
                 cell.appendChild(text);
             }
