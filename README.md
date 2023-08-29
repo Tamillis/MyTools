dcc97994e74d9f5b742ebe740d0f8c562daff593
Peter Bellaby <bellaby1993@hotmail.co.uk>
2023-08-29 22:50:07 +0100

    started work on converting TableMaker into proper FrontEnd mini-framework for gen'ing dynamic html tables

--- README.md
+++ README.md
@@ -19,3 +19,35 @@ Used during my video production for Sparta Global, in particular the HTML Short,
 A small JS utility class that allows you to create and download .csv files from JSON data.
 
 No sexy screenshot for this one, just go see the file [here](./CsvDownloader/CsvDownloader.js) and the demo [here](./CsvDownloader/Demo.html).
+
+## TableMaker
+
+The beginnings of my own front-end replacement to Telerik's Grid, giving me full control over it and also cutting down on server calls by a lot.
+
+The `TableMaker.makeTable(data, tableAttributes)` method is the only entry point so far. 
+- `data` must be valid JSON in the form of an array of objects that all have the same properties.
+- `tableAttributes` is the API that the TableMaker uses to configure the table, see below
+
+### TableMaker API
+The tableMaker functions simply: through data provided to the `tableAttributes` input, the table generated can be defined and customised.
+
+| **Attribute** | **Effect** |
+| --- | --- |
+| `id` | The html id of the table generated, defaults to `"t" + Date.now()` |
+| `classes` | the css style classes to apply. Must be an object where each parameter matches the HTML element that you want those classes to apply to, and the value an array of strings of the classes you want to apply. Note if no classes are defined for an element it defaults to the following bootstrap classes:  |
+```js 
+{
+    table : ["table", "table-striped", "table-bordered"],
+    th : ["h5"],
+    td : ["text-body-secondary"]
+}
+```
+| **Attribute** | **Effect** |
+| --- | --- |
+| `parentSelector` | the css selector for determining the parent element, defaults to `body` |
+
+TODO:
+- Add filter conditions and ordering buttons
+- Add pagination
+- manner to flag certain rows with classes / custom functionality (passed in function?)
+- flag to convert dates to short strings
--- TableMaker/TableMaker.js
+++ TableMaker/TableMaker.js
@@ -1,44 +1,114 @@
 
 class TableMaker {
+    static tableAttributeDefaults = {
+        id: "t" + Date.now(), 
+        classes: {
+            table : ["table", "table-striped", "table-bordered"],
+            th : ["h5"],
+            td : ["text-body-secondary"],
+            button: ["btn"]
+        }, 
+        parentSelector: "body",
+        sorting : false
+    };
 
-    static generateTableHead(table, data) {
+    //courtesy of https://stackoverflow.com/questions/21147832/convert-camel-case-to-human-readable-string
+    static toCapitalizedWords(name) {
+        var words = name.match(/[A-Za-z][a-z]*|[0-9]+/g) || [];
+        return words.map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(" ");
+    }
+
+    static addStylesToElement(el, styles) {
+        if(Array.isArray(styles)) for(let style of styles) el.classList.add(style);
+    }
+
+    static generateTableHead(table, data, classes) {
         let thead = table.createTHead();
         let row = thead.insertRow();
         for (let key of data) {
             let th = document.createElement("th");
-            let text = document.createTextNode(key);
+            TableMaker.addStylesToElement(th, classes.th);
+            th.value = key;
+            let text = document.createTextNode(TableMaker.toCapitalizedWords(key));
             th.appendChild(text);
             row.appendChild(th);
         }
+        return thead;
     }
-    
-    static generateTable(table, data) {
-        for (let element of data) {
-            let row = table.insertRow();
-            for (let key in element) {
-                let cell = row.insertCell();
-                let text = document.createTextNode(element[key]);
-                cell.appendChild(text);
+
+    static addSorting(head, sortings, classes, data) {
+        // thead > tr > th
+        for(let th of head.childNodes[0].childNodes) {
+            console.log(th, th.value, sortings)
+            if(sortings.includes(th.value)) {
+                let btn = document.createElement("button");
+                btn.innerText = "←";
+                btn.onclick = () => alert("TODO, the tricky bit");
+                TableMaker.addStylesToElement(btn, classes.btn);
+                th.appendChild(btn);
             }
         }
     }
 
-    static fillTable(data, tableAttributes) {
-        let table = document.getElementById(tableAttributes.id);
-        if(!table) console.error("No table found");
-        let classes = tableAttributes.classes == null ? [] : tableAttributes.classes;
-        for(let style of classes) table.classList.add(style);
-        let headings = Object.keys(data[0]);
-        TableMaker.generateTableHead(table, headings, classes);
-        TableMaker.generateTable(table, data, classes);
+    static generateTableBody(table, tableData, classes) {
+        let tbody = table.createTBody();
+        TableMaker.addStylesToElement(tbody, classes.tbody);
+        for (let rowData of tableData) {
+            TableMaker.generateRow(tbody, rowData, classes.td);
+        }
+        return tbody;
     }
 
-    static makeTable(data, parentSelector, tableAttributes = { id: "", classes: []} ) {
-        let table = document.createElement("table");
-        if(!tableAttributes || !tableAttributes.id || tableAttributes.id == "") tableAttributes.id = "t" + Date.now();
-        table.id = tableAttributes.id;
-        document.querySelector(parentSelector).appendChild(table);
+    static generateRow(tbody, data, classes) {
+        let tr = tbody.insertRow();
+        
+        for (let key in data) {
+            let td = tr.insertCell();
+            TableMaker.addStylesToElement(td, classes.td);
 
-        TableMaker.fillTable(data, tableAttributes);
+            //FORMAT DATA
+            let content = data[key];
+            if(content instanceof Date) content = content.toLocaleDateString();
+            
+            let text = document.createTextNode(content);
+            td.appendChild(text);
+        }
+        return tr;
+    }
+
+    static fillTable(table, data, tableAttributes) {
+
+        let headings = Object.keys(data[0]);
+        let head = TableMaker.generateTableHead(table, headings, tableAttributes.classes);
+        if(tableAttributes.sorting) TableMaker.addSorting(head, tableAttributes.sorting, tableAttributes.classes, data);
+
+        TableMaker.generateTableBody(table, data, tableAttributes.classes);
+
+        return table;
+    }
+
+    /// Main entry point, data must be valid and well-formed JSON
+    static makeTable(data, tableAttributes = {}) {
+        tableAttributes = {
+            ...TableMaker.tableAttributeDefaults,
+            ...tableAttributes
+        };
+
+        tableAttributes.classes = {
+            ...TableMaker.tableAttributeDefaults.classes,
+            ...tableAttributes.classes
+        }
+
+        let table = document.getElementById(tableAttributes.id);
+        if (!table) {
+            table = document.createElement("table");
+            table.id = tableAttributes.id;
+        }
+
+        for(let style of tableAttributes.classes.table) table.classList.add(style);
+
+        document.querySelector(tableAttributes.parentSelector).appendChild(table);
+
+        return TableMaker.fillTable(table, data, tableAttributes);
     }
 }
